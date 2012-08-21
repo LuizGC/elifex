40fc913e4a517c1df4338dbca21f0eadfe45c137
LuizGC <luizaugusto3b@gmail.com>
2012-08-21 00:57:23 -0300

    css adjust

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "[app_id]", "[app_secret]", :scope => 'email, user_birthday, user_education_history'
+  provider :facebook, "[app_id]", "[app_secret]", :scope => 'email, user_birthday, user_education_history, publish_stream'
 end
